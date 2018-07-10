d014bf3277e8dd15e1c66fb90aa8ae8624033a02
kendall-dev <kendallzane@gmail.com>
2018-07-10 19:50:28 -0400

    Basic top-down movement

---
+++ roguelike/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	private Vector2 movement;
+	private float hInput, vInput;
+	private Rigidbody2D rb;
+	public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hInput = Input.GetAxisRaw ("Horizontal");
+		vInput = Input.GetAxisRaw ("Vertical");
+		movement = new Vector2 (hInput, vInput).normalized * speed;
+		rb.AddForce(movement);
+	}
+}
