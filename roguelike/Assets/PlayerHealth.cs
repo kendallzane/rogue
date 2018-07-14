995253d12c102ce76807711bbf046712e42a465e
kendall-dev <kendallzane@gmail.com>
2018-07-13 21:18:38 -0400

    First scrubby experimentation with UI stuff

---
+++ roguelike/Assets/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+	public int currentHealth;
+	public int maxHealth = 3;
+	public Text healthText;
+	
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+		healthText.text = "Health: " + currentHealth + "/" + maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthText.text = "Health: " + currentHealth + "/" + maxHealth;
+	}
+	
+	
+}
