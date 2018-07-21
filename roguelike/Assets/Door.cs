62cf7ab723decace8ceac16eefb18c75ab4f36c9
kendall-dev <kendallzane@gmail.com>
2018-07-20 21:35:21 -0400

    Added room doors. AI next?

---
+++ roguelike/Assets/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	private bool looking = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!looking) {
+			return;
+		}
+		int numEnemies = 0;
+		foreach (Transform child in transform.parent) {
+			if (child.gameObject.tag == "Enemy") {
+				numEnemies++;
+			}
+		}
+		if(numEnemies <= 0) {
+			gameObject.SetActive(false);
+			looking = false;
+		}
+	}
+}
